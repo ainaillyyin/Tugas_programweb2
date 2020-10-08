2d641dba0c5d42bcfac44a65bdaefd48ab56b5b2
ainaillyyin <71957400+ainaillyyin@users.noreply.github.com>
2020-10-08 10:54:01 +0700

    Create variabel-biasa.php

---
+++ variabel-biasa.php
@@ -0,0 +1,17 @@
+<?php  
+$nama = "Aina Illyyin";
+$nim = 2018804475;
+$jurusan = "Sistem Informasi";
+$kampus = "STMIK INSAN PEMBANGUNAN";
+
+echo "Biodata Diri";
+echo "<br>";
+echo "Nama Mahasiswa : $nama";
+echo "<br>";
+echo "NIM Mahasiswa : $nim";
+echo "<br>";
+echo "Jurusan : $jurusan";
+echo "<br>";
+echo "Nama Kampus : $kampus";
+
+?>
